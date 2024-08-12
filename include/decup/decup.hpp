eac736d8a87c2a6447dd999fd90bfd936b1ea578
Vincent Hamp <vincent.hamp@higaski.at>
2024-08-12 15:29:56 +0200

    docs: add old docs

---
+++ include/decup/decup.hpp
@@ -0,0 +1,13 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/// DECUP
+///
+/// \file   decup/decup.hpp
+/// \author Vincent Hamp
+/// \date   12/08/2024
+
+#pragma once
+
+#include "decup/packet.hpp"
